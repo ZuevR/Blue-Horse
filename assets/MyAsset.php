3c820998b027e20cfb0420a9a697a13508c40020
Zuev Roman <ZuevRG@yandex.ru>
2019-07-29 01:44:08 +0300

    Added MyAsset and gulp

---
+++ assets/MyAsset.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Main application asset bundle.
+ *
+ * @author Zuev Roman
+ *
+ */
+class MyAsset extends AssetBundle
+{
+  public $basePath = '@webroot';
+  public $baseUrl = '@web';
+  public $css = [
+    'css/style.css'
+  ];
+  public $js = [
+  ];
+  public $depends = [
+    'yii\web\YiiAsset',
+  ];
+}
